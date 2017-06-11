LineageOS/android_device_samsung_jf-common

jf: Generate graphics vendor Makefile if missing
This allows to build CM without cloning vendor repos.

Change-Id: I87777485edc5f29c5899091176e55d28e36bb263

setup-makefiles.sh
@@ -39,7 +39,11 @@ write_headers "jactivelte jflteatt jfltespr jfltetmo jfltevzw jfltexx jfltecan j
 
 write_makefiles "$MY_DIR"/common-proprietary-files.txt
 
-printf '\n%s\n' "\$(call inherit-product, vendor/qcom/binaries/msm8960/graphics/graphics-vendor.mk)" >> "$PRODUCTMK"
+if [ -s "$CM_ROOT"/vendor/qcom/binaries/msm8974/graphics/graphics-vendor.mk ]; then
+    printf '\n%s\n' "\$(call inherit-product, vendor/qcom/binaries/msm8960/graphics/graphics-vendor.mk)" >> "$PRODUCTMK"
+else
+    write_makefiles "$MY_DIR"/../../qcom/common/extractors/graphics-msm8960.txt
+fi
 
 write_footers
 

LineageOS/android_device_samsung_jfvelte

setup-makefiles.sh
@@ -39,7 +39,11 @@ write_headers "jactivelte jflteatt jfltespr jfltetmo jfltevzw jfltexx jfltecan j
 
 write_makefiles "$MY_DIR"/common-proprietary-files.txt
 
-printf '\n%s\n' "\$(call inherit-product, vendor/qcom/binaries/msm8960/graphics/graphics-vendor.mk)" >> "$PRODUCTMK"
+if [ -s "$CM_ROOT"/vendor/qcom/binaries/msm8960/graphics/graphics-vendor.mk ]; then
+    printf '\n%s\n' "\$(call inherit-product, vendor/qcom/binaries/msm8960/graphics/graphics-vendor.mk)" >> "$PRODUCTMK"
+else
+    write_makefiles "$MY_DIR"/../../qcom/common/extractors/graphics-msm8960.txt
+fi
 
 write_footers
 