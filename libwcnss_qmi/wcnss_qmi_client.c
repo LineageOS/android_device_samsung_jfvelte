a1ed52e8185df3395fcc7b802394fc724e02c32e
Arne Coucheron <arco68@gmail.com>
2019-07-15 06:30:57 +0200

    jfvelte: Initial import of rebased tree
    Change-Id: Ib6df2470df9f33ea86ec16c2847df9efad782d3b

--- libwcnss_qmi/wcnss_qmi_client.c
+++ libwcnss_qmi/wcnss_qmi_client.c
@@ -1,5 +1,6 @@
 /*
- * Copyright (C) 2015, The CyanogenMod Project
+ * Copyright (C) 2013-2016, The CyanogenMod Project
+ * Copyright (C) 2017, The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -23,27 +24,26 @@
 
 #define MAC_INFO_FILE "/efs/wifi/.mac.info"
 
-#include <cutils/log.h>
+#include <log/log.h>
 #include <stdio.h>
 
-int wcnss_init_qmi(void)
-{
+int wcnss_init_qmi(void) {
     /* empty */
     return SUCCESS;
 }
 
-int wcnss_qmi_get_wlan_address(unsigned char *mac)
-{
+int wcnss_qmi_get_wlan_address(unsigned char* mac) {
     int i;
     int tmp[6];
-    FILE *f;
+    FILE* f;
 
     if ((f = fopen(MAC_INFO_FILE, "r")) == NULL) {
         ALOGE("%s: failed to open %s", __func__, MAC_INFO_FILE);
         return FAILED;
     }
 
-    if (fscanf(f, "%02X:%02X:%02X:%02X:%02X:%02X", &tmp[0], &tmp[1], &tmp[2], &tmp[3], &tmp[4], &tmp[5]) != 6) {
+    if (fscanf(f, "%02X:%02X:%02X:%02X:%02X:%02X", &tmp[0], &tmp[1], &tmp[2], &tmp[3], &tmp[4],
+               &tmp[5]) != 6) {
         ALOGE("%s: %s: file contents are not valid", __func__, MAC_INFO_FILE);
         fclose(f);
         return FAILED;
@@ -55,7 +55,6 @@ int wcnss_qmi_get_wlan_address(unsigned char *mac)
     return SUCCESS;
 }
 
-void wcnss_qmi_deinit(void)
-{
+void wcnss_qmi_deinit(void) {
     /* empty */
 }
